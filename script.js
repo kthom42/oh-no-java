54dca7d33b0faab14aa175d4e9281a213d7d6b6f
kthom42 <kmgt42@gmail.com>
2022-10-03 19:31:40 -0400

    added password criteria for generator

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+const specialCharacters = "!@#$%^&*()";
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +11,17 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+var passwordLenth("Enter the amount of characters you would like for you password. It must be between 8 and 128 characters")
+var lowerCase("Would you like lowercase letters in your password")
+var uppperCase("Would you like uppercase letters in your password")
+var numers("Would you like numbers to be included in your password")
+var specialCharacters("Would you like special characters to be included in your password")
+
+
+
+
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
